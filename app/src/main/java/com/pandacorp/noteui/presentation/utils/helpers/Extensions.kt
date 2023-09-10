3dd2690adf430ffc466f6d176f29342132ab4c60
MrRuslanYT <pandacorpglobal@gmail.com>
2023-09-10 13:17:54 +0300

    Added JavaDoc for the `Drawable.animateAlpha` and `View.animateAlpha` functions

--- app/src/main/java/com/pandacorp/noteui/presentation/utils/helpers/Extensions.kt
+++ app/src/main/java/com/pandacorp/noteui/presentation/utils/helpers/Extensions.kt
@@ -32,6 +32,16 @@ fun Toolbar.hideToolbarWhileScrollingUseCase(isHide: Boolean) {
     this.layoutParams = layoutParams
 }
 
+/**
+ * Animates the alpha (transparency) property of a Drawable from one value to another over a specified duration.
+ *
+ * @param fromAlpha The starting alpha value (0 for fully transparent, 255 for fully opaque).
+ * @param toAlpha The ending alpha value (0 for fully transparent, 255 for fully opaque).
+ * @param duration The duration of the animation in milliseconds.
+ * @param onAnimationEnd A callback function to be invoked when the animation ends (optional).
+ *
+ * @throws IllegalArgumentException if [fromAlpha] or [toAlpha] is not within the range [0, 255].
+ */
 fun Drawable.animateAlpha(fromAlpha: Int, toAlpha: Int, duration: Long, onAnimationEnd: (() -> Unit)? = null) {
     val animator = ValueAnimator.ofInt(fromAlpha, toAlpha).apply {
         this.duration = duration
@@ -52,6 +62,18 @@ fun Drawable.animateAlpha(fromAlpha: Int, toAlpha: Int, duration: Long, onAnimat
     animator.start()
 }
 
+/**
+ * Animates the alpha (transparency) property of a View from one value to another over a specified duration.
+ *
+ * @param isAnimating Indicates whether the animation should be performed, if the value is false
+ * then toAlpha is assigned immediately and onAnimationEnd called.
+ * @param fromAlpha The starting alpha value (0.0f for transparent, 1.0f for opaque).
+ * @param toAlpha The ending alpha value (0.0f for transparent, 1.0f for opaque).
+ * @param duration The duration of the animation in milliseconds.
+ * @param onAnimationEnd A callback function to be invoked when the animation ends (optional).
+ *
+ * @throws IllegalArgumentException if [fromAlpha] or [toAlpha] is not within the range [0.0f, 1.0f].
+ */
 fun View.animateAlpha(
     isAnimating: Boolean = true,
     fromAlpha: Float,
