063e860724e6fa7d1f151399636a797282d6cecd
MrRuslanYT <pandacorpglobal@gmail.com>
2023-10-11 15:12:14 +0300

    Fixed wrong AnimatedTextView measurements of text that resulted in showing ellipsized text instead of the text

--- AnimatedTextView/src/main/java/com/pandacorp/animatedtextview/AndroidUtilities.kt
+++ AnimatedTextView/src/main/java/com/pandacorp/animatedtextview/AndroidUtilities.kt
@@ -1,6 +1,11 @@
 package com.pandacorp.animatedtextview
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
 import android.graphics.Point
+import android.view.WindowManager
+import kotlin.math.abs
 import kotlin.math.ceil
 
 object AndroidUtilities {
@@ -34,6 +39,38 @@ object AndroidUtilities {
             0
         } else {
             ceil((density * value).toDouble()).toInt()
+        }
+    }
+
+    fun checkDisplaySize(
+        activity: Activity,
+        newConfiguration: Configuration?
+    ) {
+        try {
+            density = activity.resources.displayMetrics.density
+            var configuration: Configuration? = newConfiguration
+            if (configuration == null) {
+                configuration = activity.resources.configuration
+            }
+
+            val manager = activity.getSystemService(Context.WINDOW_SERVICE) as? WindowManager
+            manager?.defaultDisplay?.getSize(displaySize)
+            if (configuration == null) {
+                return
+            }
+            if (configuration.screenWidthDp !== Configuration.SCREEN_WIDTH_DP_UNDEFINED) {
+                val newSize = ceil(configuration.screenWidthDp * density.toDouble()).toInt()
+                if (abs(displaySize.x - newSize) > 3) {
+                    displaySize.x = newSize
+                }
+            }
+            if (configuration.screenHeightDp !== Configuration.SCREEN_HEIGHT_DP_UNDEFINED) {
+                val newSize = ceil(configuration.screenHeightDp * density.toDouble()).toInt()
+                if (abs(displaySize.y - newSize) > 3) {
+                    displaySize.y = newSize
+                }
+            }
+        } catch (_: Exception) {
         }
     }
 }
