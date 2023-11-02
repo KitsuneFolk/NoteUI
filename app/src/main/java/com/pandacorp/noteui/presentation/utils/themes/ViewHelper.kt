27bdee49c57b9c95205b3a13ab574212deda5956
MrRuslanYT <pandacorpglobal@gmail.com>
2023-11-02 16:34:38 +0200

    Added textview textColor change

--- app/src/main/java/com/pandacorp/noteui/presentation/utils/themes/ViewHelper.kt
+++ app/src/main/java/com/pandacorp/noteui/presentation/utils/themes/ViewHelper.kt
@@ -1,11 +1,13 @@
 package com.pandacorp.noteui.presentation.utils.themes
 
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.view.children
 import com.dolatkia.animatedThemeManager.AppTheme
 
 object ViewHelper {
     lateinit var currentTheme: AppTheme
+    private val themes = mutableListOf(BlueTheme(), DarkTheme(), LightTheme(), PurpleTheme())
 
     fun applyTheme(
         newTheme: AppTheme,
@@ -20,11 +22,25 @@ object ViewHelper {
     ) {
         val context = viewGroup.context
         val views = viewGroup.children
+        val themesTextColor = themes.map { it.getTextColor() }
+        val themesTextColorSecondary = themes.map { it.getTextColorSecondary(context) }
 
         for (view in views) {
             if (view is ViewGroup) {
-                view.setBackgroundColor(newTheme.getColorBackground(context))
                 applyTheme(newTheme, view)
+                view.setBackgroundColor(newTheme.getColorBackground(context))
+            }
+
+            if (view is TextView) {
+                when (view.currentTextColor) {
+                    in themesTextColor -> {
+                        view.setTextColor(newTheme.getTextColor())
+                    }
+
+                    in themesTextColorSecondary -> {
+                        view.setTextColor(newTheme.getTextColorSecondary(context))
+                    }
+                }
             }
 
             if (view is androidx.appcompat.widget.Toolbar) {
