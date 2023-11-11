47483c0ba12b53df32702ab231a9a708f6e65eb4
MrRuslanYT <pandacorpglobal@gmail.com>
2023-11-11 13:08:16 +0200

    Removed tests in NumberPicker

--- NumberPicker/build.gradle.kts
+++ NumberPicker/build.gradle.kts
@@ -9,7 +9,6 @@ android {
     defaultConfig {
         minSdk = 21
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -26,10 +25,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
