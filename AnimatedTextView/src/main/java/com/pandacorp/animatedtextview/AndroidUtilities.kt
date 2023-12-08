cc14a61cd60dfb63f16ae9b610cbd18c37b2b97e
MrRuslanYT <pandacorpglobal@gmail.com>
2023-12-08 12:41:35 +0200

    Resolved code warnings in AndroidUtilities.kt

--- AnimatedTextView/src/main/java/com/pandacorp/animatedtextview/AndroidUtilities.kt
+++ AnimatedTextView/src/main/java/com/pandacorp/animatedtextview/AndroidUtilities.kt
@@ -3,6 +3,7 @@ package com.pandacorp.animatedtextview
 import android.content.Context
 import android.content.res.Configuration
 import android.graphics.Point
+import android.os.Build
 import android.view.WindowManager
 import kotlin.math.abs
 import kotlin.math.ceil
@@ -53,17 +54,26 @@ object AndroidUtilities {
             }
 
             val manager = context.getSystemService(Context.WINDOW_SERVICE) as? WindowManager
-            manager?.defaultDisplay?.getSize(displaySize)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                manager?.currentWindowMetrics?.bounds?.let {
+                    displaySize.x = it.width()
+                    displaySize.y = it.height()
+                }
+            } else {
+                @Suppress("DEPRECATION")
+                manager?.defaultDisplay?.getSize(displaySize)
+            }
             if (configuration == null) {
                 return
             }
-            if (configuration.screenWidthDp !== Configuration.SCREEN_WIDTH_DP_UNDEFINED) {
+            if (configuration.screenWidthDp != Configuration.SCREEN_WIDTH_DP_UNDEFINED) {
                 val newSize = ceil(configuration.screenWidthDp * density.toDouble()).toInt()
                 if (abs(displaySize.x - newSize) > 3) {
                     displaySize.x = newSize
                 }
             }
-            if (configuration.screenHeightDp !== Configuration.SCREEN_HEIGHT_DP_UNDEFINED) {
+            if (configuration.screenHeightDp != Configuration.SCREEN_HEIGHT_DP_UNDEFINED) {
                 val newSize = ceil(configuration.screenHeightDp * density.toDouble()).toInt()
                 if (abs(displaySize.y - newSize) > 3) {
                     displaySize.y = newSize
